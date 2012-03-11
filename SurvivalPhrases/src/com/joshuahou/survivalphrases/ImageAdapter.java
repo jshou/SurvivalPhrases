32c805543cbb8868246deb79d08bba9ea81ff948
jshou <joshuahou@gmail.com>
2012-03-11 00:42:42 -0800

    title bar, grid of images, removed R.java from index

---
+++ SurvivalPhrases/src/com/joshuahou/survivalphrases/ImageAdapter.java
@@ -0,0 +1,64 @@
+package com.joshuahou.survivalphrases;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class ImageAdapter extends BaseAdapter {
+    private Context context;
+
+    public ImageAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return thumbIds.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return thumbIds[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {  // if it's not recycled, initialize some attributes
+            imageView = new ImageView(context);
+            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+            imageView.setPadding(8, 8, 8, 8);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageResource(thumbIds[position]);
+        return imageView;
+    }
+
+    // references to our images
+    private Integer[] thumbIds = {
+            R.drawable.translate,
+            R.drawable.translate,
+            R.drawable.translate,
+            R.drawable.translate,
+            R.drawable.translate,
+            R.drawable.translate,
+            R.drawable.translate,
+            R.drawable.translate,
+            R.drawable.translate,
+            R.drawable.translate,
+            R.drawable.translate,
+            R.drawable.translate,
+    };
+}
--- SurvivalPhrases/src/com/joshuahou/survivalphrases/Main.java
+++ SurvivalPhrases/src/com/joshuahou/survivalphrases/Main.java
@@ -2,14 +2,20 @@ package com.joshuahou.survivalphrases;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.GridView;
 
-public class Main extends Activity
-{
+public class Main extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
         setContentView(R.layout.main);
+        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.window_title);
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
     }
 }
