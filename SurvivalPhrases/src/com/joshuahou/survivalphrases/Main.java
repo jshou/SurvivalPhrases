7f13a32874929bdd3dd602f82fe18526c51777c0
jshou <joshuahou@gmail.com>
2012-03-11 18:06:38 -0700

    better layout, added spinner

--- SurvivalPhrases/src/com/joshuahou/survivalphrases/Main.java
+++ SurvivalPhrases/src/com/joshuahou/survivalphrases/Main.java
@@ -3,7 +3,9 @@ package com.joshuahou.survivalphrases;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.Spinner;
 
 public class Main extends Activity {
     /** Called when the activity is first created. */
@@ -11,12 +13,18 @@ public class Main extends Activity {
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
+//        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
         setContentView(R.layout.main);
-        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.window_title);
+//        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.window_title);
 
         GridView gridview = (GridView) findViewById(R.id.gridview);
         gridview.setAdapter(new ImageAdapter(this));
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.language_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 
     @Override
